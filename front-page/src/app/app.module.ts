8edc019f614f3efbb8de23ea57fb7ccd059c5108
Max Couch <caxmouch@gmail.com>
2019-01-02 21:02:20 -0500

    broken front page

--- front-page/src/app/app.module.ts
+++ front-page/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { DialogueComponent } from './dialogue/dialogue.component';
 import { VitalsComponent } from './vitals/vitals.component';
 import { ClipboardComponent } from './clipboard/clipboard.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DialogueComponent,
     VitalsComponent,
-    ClipboardComponent
+    ClipboardComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
