0b34085a9b4ff343d53071d9951af289bf312b6c
Jacob Mulrenin <jmulrenin@gmail.com>
2019-01-07 10:26:37 -0500

    Comments on angular project

--- front-page/src/app/app.component.ts
+++ front-page/src/app/app.component.ts
@@ -1,3 +1,9 @@
+/*@author Jake Mulrenin, Max Couch*/
+/**
+ * The ts for the app component is found here. It contains a title 
+ * for the front page as well as importing globals to access the global
+ * variables and functions.
+ */
 import { Component } from '@angular/core';
 import { Globals } from './global';
 
