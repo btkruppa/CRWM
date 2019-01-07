c3f7b02eb7b2b2b9eaf425a2fbb702a24ab5b7ba
Jacob Mulrenin <jmulrenin@gmail.com>
2019-01-07 15:02:51 -0500

    yet another player bean update

--- MedicalRPG/src/main/java/com/rev/beans/Player.java
+++ MedicalRPG/src/main/java/com/rev/beans/Player.java
@@ -31,12 +31,12 @@ public class Player {
 public Player(int player_ID, String username, String password, int score, String firstname, String lastname,
 		String isdev, LeaderBoard leader) {
 		super();
-		Player_ID = player_ID;
-		Username = username;
-		Password = password;
-		Score = score;
-		Firstname = firstname;
-		Lastname = lastname;
+		this.player_ID = player_ID;
+		this.username = username;
+		this.password = password;
+		this.score = score;
+		this.firstname = firstname;
+		this.lastname = lastname;
 		this.isdev = isdev;
 		this.leader = leader;
 	}
@@ -44,17 +44,17 @@ public Player(int player_ID, String username, String password, int score, String
 	/*@GeneratedValue(strategy=GenerationType.AUTO, generator="playerSequence")
 	@SequenceGenerator(allocationSize= 1,name="playerSequence",sequenceName="SQ_PLAYER_PK")*/
 	@Column(name="PLAYER_ID")
-	private int Player_ID;
+	private int player_ID;
 	@Column(name="USERNAME")
-	private String Username;
+	private String username;
 	@Column(name="PLAYER_PASSWORD")
-	private String Password;
+	private String password;
 	@Column(name="HIGH_SCORES")
-	private int Score;
+	private int score;
 	@Column(name="FIRST_NAME")
-	private String Firstname;
+	private String firstname;
 	@Column(name="LAST_NAME")
-	private String Lastname;
+	private String lastname;
 	@Column(name="IS_DEV")
 	private String isdev;
 	@ManyToOne
@@ -62,40 +62,40 @@ public Player(int player_ID, String username, String password, int score, String
 	private LeaderBoard leader;
 	
 	public int getId() {
-		return Player_ID;
+		return player_ID;
 	}
 	public void setId(int id) {
-		this.Player_ID = id;
+		this.player_ID = id;
 	}
 	public String getUsername() {
-		return Username;
+		return username;
 	}
 	public void setUsername(String username) {
-		Username = username;
+		this.username = username;
 	}
 	public String getPassword() {
-		return Password;
+		return password;
 	}
 	public void setPassword(String password) {
-		Password = password;
+		this.password = password;
 	}
 	public int getScore() {
-		return Score;
+		return score;
 	}
 	public void setScore(int score) {
-		Score = score;
+		this.score = score;
 	}
 	public String getFirstname() {
-		return Firstname;
+		return firstname;
 	}
 	public void setFirstname(String firstname) {
-		Firstname = firstname;
+		this.firstname = firstname;
 	}
 	public String getLastname() {
-		return Lastname;
+		return lastname;
 	}
 	public void setLastname(String lastname) {
-		Lastname = lastname;
+		this.lastname = lastname;
 	}
 	public String getIsdev() {
 		return isdev;
@@ -104,80 +104,9 @@ public Player(int player_ID, String username, String password, int score, String
 		this.isdev = isdev;
 	}
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((Firstname == null) ? 0 : Firstname.hashCode());
-		result = prime * result + ((Lastname == null) ? 0 : Lastname.hashCode());
-		result = prime * result + ((Password == null) ? 0 : Password.hashCode());
-		result = prime * result + Player_ID;
-		result = prime * result + Score;
-		result = prime * result + ((Username == null) ? 0 : Username.hashCode());
-		result = prime * result + ((isdev == null) ? 0 : isdev.hashCode());
-		result = prime * result + ((leader == null) ? 0 : leader.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Player other = (Player) obj;
-		if (Firstname == null) {
-			if (other.Firstname != null)
-				return false;
-		} else if (!Firstname.equals(other.Firstname))
-			return false;
-		if (Lastname == null) {
-			if (other.Lastname != null)
-				return false;
-		} else if (!Lastname.equals(other.Lastname))
-			return false;
-		if (Password == null) {
-			if (other.Password != null)
-				return false;
-		} else if (!Password.equals(other.Password))
-			return false;
-		if (Player_ID != other.Player_ID)
-			return false;
-		if (Score != other.Score)
-			return false;
-		if (Username == null) {
-			if (other.Username != null)
-				return false;
-		} else if (!Username.equals(other.Username))
-			return false;
-		if (isdev == null) {
-			if (other.isdev != null)
-				return false;
-		} else if (!isdev.equals(other.isdev))
-			return false;
-		if (leader == null) {
-			if (other.leader != null)
-				return false;
-		} else if (!leader.equals(other.leader))
-			return false;
-		return true;
-	}
-	@Override
 	public String toString() {
-		return "Player [Player_ID=" + Player_ID + ", Username=" + Username + ", Password=" + Password + ", Score="
-				+ Score + ", Firstname=" + Firstname + ", Lastname=" + Lastname + ", isdev=" + isdev + ", leader="
+		return "Player [Player_ID=" + player_ID + ", Username=" + username + ", Password=" + password + ", Score="
+				+ score + ", Firstname=" + firstname + ", Lastname=" + lastname + ", isdev=" + isdev + ", leader="
 				+ leader + "]";
-	}
-	public synchronized int getPlayer_ID() {
-		return Player_ID;
-	}
-	public synchronized void setPlayer_ID(int player_ID) {
-		Player_ID = player_ID;
-	}
-	public synchronized LeaderBoard getLeader() {
-		return leader;
-	}
-	public synchronized void setLeader(LeaderBoard leader) {
-		this.leader = leader;
 	}
 }
