881e42eb05896af5870456e8bd1c37b9d0d89b83
RuizR <aln.rzv@gmail.com>
2019-01-10 18:32:29 -0500

    start mockito

---
+++ MedicalRPG/src/test/java/com/rev/test/MockitoTest.java
@@ -0,0 +1,17 @@
+/**
+ * @author Rebecca Ruiz
+ */
+package com.rev.test;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoTest {
+	
+	//@Mock 
+	//@Rule public MockitoRule rule = MockitoJUnit rule();
+
+}
